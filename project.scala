31324e3aee26f70e260c4059a06bbd46cdf14440
Riley <rh1122@ic.ac.uk>
2024-04-19 21:17:10 +0100

    [added] project.scala clarification

--- project.scala
+++ project.scala
@@ -5,6 +5,7 @@
 //> using dep com.github.j-mie6::parsley::5.0.0-M6
 //> using test.dep org.scalatest::scalatest::3.2.18
 
+// compiler options
 //> using options -deprecation -unchecked -feature
 //> using options -Xlint:nullary-unit -Xlint:infer-any -Xlint:unused -Xlint:nonlocal-return
 //> using options -Wdead-code -Wextra-implicit -Wnumeric-widen
